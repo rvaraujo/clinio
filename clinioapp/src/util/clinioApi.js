467f82b11a4efa1722672957fe81c03b16479409
Rafael Vieira <rafael@cloudmob.com.br>
2020-06-12 09:03:50 -0300

    teste deploy

--- clinioapp/src/util/clinioApi.js
+++ clinioapp/src/util/clinioApi.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export default axios.create({baseURL: "https://localhost:5001/", responseType:"application/json"});
+export default axios.create({baseURL: "https://clinioapi.herokuapp.com/", responseType:"application/json"});
